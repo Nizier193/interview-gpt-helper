732af92052ddf3c0b6402a40dcbfc43e9c2b8c56
Nizier193 <nikita.borisov07@gmail.com>
2024-12-11 23:32:44 +0300

    added LLM Module

--- services/llms/main.py
+++ services/llms/main.py
@@ -1,4 +1,29 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
 
 class LLMModule():
+    def __init__(self) -> None:
+        self.client = openai.OpenAI()
+
     def get_response(self, prompt: str) -> str:
-        return "Dummy"
+        completion = self.client.chat.completions.create(
+            model="model-dumb",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ]
+        )
+
+        if len(completion.choices) == 0:
+            return ""
+        else:
+            return completion.choices[-1].message.content
+        
+
+module = LLMModule()
+print(module.get_response("Hi!"))
